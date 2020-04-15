9bbb090ee21bb48091c0eb43dbd8731d0d2571fe
Alex Panas <alexpanasucla@gmail.com>
2020-04-15 19:01:47 +0300

    add script to upload image to dockerhub

--- upload_docker.sh
+++ upload_docker.sh
@@ -6,10 +6,14 @@
 # Step 1:
 # Create dockerpath
 # dockerpath=<your docker ID/path>
+dockerpath='dockerpanas/cloundnd_4'
 
 # Step 2:  
 # Authenticate & tag
+docker login 
+docker tag api $dockerpath
 echo "Docker ID and Image: $dockerpath"
 
 # Step 3:
 # Push image to a docker repository
+docker push $dockerpath
